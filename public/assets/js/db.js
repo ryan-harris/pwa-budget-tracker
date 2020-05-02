3a23e92e0eb2395afa3ecd23ac15e28d165a5f54
Ryan Harris <harris.ucla@gmail.com>
2020-05-01 23:46:28 -0700

    Handle offline transactions using indexedDB

--- public/assets/js/db.js
+++ public/assets/js/db.js
@@ -1,26 +1,65 @@
-
-// TODO: open  indexedDB
 const indexedDB =
-window.indexedDB ||
-window.mozIndexedDB ||
-window.webkitIndexedDB ||
-window.msIndexedDB ||
-window.shimIndexedDB;
+  window.indexedDB ||
+  window.mozIndexedDB ||
+  window.webkitIndexedDB ||
+  window.msIndexedDB ||
+  window.shimIndexedDB;
 
 let db;
 const request = indexedDB.open("budget", 1);
 
-// TODO: create an object store in the open db
+request.onupgradeneeded = (event) => {
+  event.target.result.createObjectStore("pending", {
+    keyPath: "id",
+    autoIncrement: true
+  });
+};
 
-// TODO: log any indexedDB errors
+request.onerror = (err) => {
+  console.log(err.message);
+};
 
-// TODO: add code so that any transactions stored in the db
-// are sent to the backend if/when the user goes online
-// Hint: learn about "navigator.onLine" and the "online" window event.
+request.onsuccess = (event) => {
+  db = event.target.result;
 
-// TODO: add code to saveRecord so that it accepts a record object for a
-// transaction and saves it in the db. This function is called in index.js
+  if (navigator.onLine) {
+    checkDatabase();
+  }
+};
+
+// This function is called in index.js
 // when the user creates a transaction while offline.
 function saveRecord(record) {
-  // add your code here
+  const transaction = db.transaction("pending", "readwrite");
+  const store = transaction.objectStore("pending");
+  store.add(record);
 }
+
+// called when user goes online to send transactions stored in db to server
+function checkDatabase() {
+  const transaction = db.transaction("pending", "readonly");
+  const store = transaction.objectStore("pending");
+  const getAll = store.getAll();
+
+  getAll.onsuccess = () => {
+    if (getAll.result.length > 0) {
+      fetch("/api/transaction/bulk", {
+        method: "POST",
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json"
+        }
+      })
+        .then((response) => response.json())
+        .then(() => {
+          const transaction = db.transaction("pending", "readwrite");
+          const store = transaction.objectStore("pending");
+          store.clear();
+        });
+    }
+  };
+}
+
+// listen for app coming back online
+window.addEventListener("online", checkDatabase);
